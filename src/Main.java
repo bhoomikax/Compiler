13835e449ffad5c2e18d109cb1e03a8be1194f87
bhoomika <bhoomikakum06@gmail.com>
2026-09-16 16:10:53 +0530

    feat: generate working x86-64 executable with output

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import lexer.Lexer;
 import lexer.Token;
 import parser.Parser;
 import semantic.SemanticAnalyzer;
+import codegen.CodeGenerator;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
@@ -11,36 +14,69 @@ public class Main {
     public static void main(String[] args) {
 
         String source = """
-                func int add(int a, int b) {
-                    send a + b;
-                }
-
                 func int main() {
 
-                    int x = 10;
-                    int y = 20;
+                int x = 10;
+                int y = 20;
 
-                    int result = add(x , y);
+                int result = x + y;
 
-                    output(result);
+                output(result);
 
-                    send result;
+                send result;
                 }
                 """;
+
+        // 1. LEXER
 
         Lexer lexer = new Lexer(source);
         List<Token> tokens = lexer.tokenize();
 
+      
+        // 2. PARSER
+
         Parser parser = new Parser(tokens);
         Program program = parser.parse();
+
+        
+        // 3. SEMANTIC ANALYSIs
 
         SemanticAnalyzer analyzer =
                 new SemanticAnalyzer();
 
         analyzer.analyze(program);
 
-        System.out.println(
-                "Semantic analysis completed."
-        );
+        
+        // 4. CODE GENERATION
+
+        CodeGenerator generator =
+                new CodeGenerator();
+
+        String assembly =
+                generator.generate(program);
+
+                
+        // 5. WRITE ASSEMBLY FILE
+        
+
+        String outputFile = "out/program.s";
+
+        try (FileWriter writer =
+                     new FileWriter(outputFile)) {
+
+            writer.write(assembly);
+
+            System.out.println(
+                    "Assembly generated: "
+                            + outputFile
+            );
+
+        } catch (IOException e) {
+
+            System.err.println(
+                    "Failed to write assembly: "
+                            + e.getMessage()
+            );
+        }
     }
 }
